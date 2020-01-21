53b81189c6edf8428aadb30aafdd018d703d5c14
Volker Goehler <volker.goehler@informatik.tu-freiberg.de>
2020-01-21 23:02:49 +0100

    debuged cli

--- src/i3_battery_block/cli.py
+++ src/i3_battery_block/cli.py
@@ -17,34 +17,36 @@ Why does this file exist, and why not put this in __main__?
 """
 import argparse
 import logging
-import os.path
-from typing import IO
 
 
 class CLI:
     """The command line interface"""
 
     def __init__(self):
-        parser = argparse.ArgumentParser(description='CLI for i3 wm block battery',
-                                         epilog='volker.goehler@informatik.tu-freiberg.de'
-                                         )
-        parser.add_argument('-c', '--compact', action='store_true' help="flag for compact mode, truncates all batteries into one") #TODO implement this
-        parser.add_argument('-l', '--loglevel', help="log level", choices=["debug", "info", "warning", "error", "critical"],
-                              type=str,
-                              default="warning")
+        self.args = None
 
+        self.parser = argparse.ArgumentParser(description='CLI for i3 wm block battery',
+                                              epilog='volker.goehler@informatik.tu-freiberg.de'
+                                              )
+        self.parser.add_argument('-c', '--compact', action='store_true',
+                                 help="flag for compact mode, truncates all batteries into one")
+        # TODO implement this
+        self.parser.add_argument('-l', '--loglevel', help="log level",
+                                 choices=["debug", "info", "warning", "error", "critical"],
+                                 type=str,
+                                 default="warning")
 
-    def parse(self, args : list):
-        self.args = parser.parse_args(args=args)
+    def parse(self, args: list):
+        self.args = self.parser.parse_args(args=args)
 
     @property
     def get_log_level(self) -> int:
         """ parses the input loglevel to the numeric value """
-        return getattr(logging, self.args.logging.upper(), None)
+        return getattr(logging, self.args.loglevel.upper().strip(), None)
 
     @property
     def is_compact(self) -> bool:
-        return getattr(Compact, self.args.compact), None)
+        return self.args.compact
 
 
 def main(args=None):
@@ -54,5 +56,4 @@ def main(args=None):
     logging.basicConfig(level=cli.get_log_level)
 
     # TODO output logic here
-    print(cli.args.names)
-
+    print(cli.args)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,29 @@
+import argparse
+import logging
+import pytest
+
 from i3_battery_block import cli
 
-def smoke_test_compact():
-    cli.main(['-c'])
 
-def smoke_test_loglevel():
-    cli.main(['-l warning'])
+def test_compact():
+    sut = cli.CLI()
+    sut.parse(args=['-c'])
+    assert sut.args.compact is True, "Compact flag should be true"
+
+
+def test_compact_negated():
+    sut = cli.CLI()
+    sut.parse(args=[])
+    assert sut.args.compact is False, "Compact flag should be false"
+
+
+def test_log_level_set():
+    sut = cli.CLI()
+    sut.parse(args=['-l=warning'])
+    assert sut.get_log_level == logging.WARNING, "log level should be warning"
+
+
+def test_log_level_wrong():
+    sut = cli.CLI()
+    with pytest.raises(SystemExit):
+        sut.parse(args=['--loglevel=fubar'])
