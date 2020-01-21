7d3abfe72a1300cfec08e39549f12b62fd7863fc
Volker Goehler <volker.goehler@informatik.tu-freiberg.de>
2020-01-21 23:04:01 +0100

    changed color and reorganized file

--- src/i3_battery_block/battery.py
+++ src/i3_battery_block/battery.py
@@ -10,14 +10,58 @@ import re
 from subprocess import check_output
 
 
-def wrap_span(text: str, color: str = None) -> str:
+def wrap_span(text: str, col: str = None) -> str:
     """
     :type text: string, the text that should be wraped in the span
-    :type color: string, a html color string
+    :type col: string, a html color string
     """
-    color_text = "color='%s'" % color if color else ""
-    return "<span font='FontAwesome' %s >%s</span>" % (color_text, text)
+    color_text = " col='%s'" % col if col else ""
+    return "<span font='FontAwesome'%s>%s</span>" % (color_text, text)
 
+
+def color(percent):
+    if percent < 0 or percent > 100:
+        raise AttributeError("Threshold %i is out of percent range." % percent)
+
+    # we only need the first number
+    deca = percent // 10
+    return __COLOR_MAP[deca]
+
+
+# as exit code 33 will turn background red 0-9 percent font is white
+__COLOR_MAP = {
+    0: "#FFFFFF",
+    1: "#FF3300",
+    2: "#FF6600",
+    3: "#FF9900",
+    4: "#FFCC00",
+    5: "#FFFF00",
+    6: "#FFFF33",
+    7: "#FFFF66",
+    8: "#FFFFFF",
+    9: "#FFFFFF",
+    10: "#FFFFFF",
+}
+
+# stands for charging
+FA_LIGHTNING = wrap_span("\uf0e7", "yellow")
+
+# stands for plugged in
+FA_PLUG = wrap_span('\uf1e6')
+
+# stands for using battery
+FA_BATTERY = wrap_span('\uf240')
+
+# stands for unknown status of battery
+FA_QUESTION = wrap_span('\uf128')
+
+FA_BATTERY_LIST = [
+    "\uf244",  # empty
+    "\uf243",  # 1 quarter
+    "\uf242",  # half
+    "\uf241",  # 3 quarters
+    "\uf240",  # full
+]
 
 status = check_output(['acpi'], universal_newlines=True)
 
@@ -26,27 +70,27 @@ if not status:
     fulltext = wrap_span("\uf00d \uf240", "red")
     percentleft = 100
 else:
-    # if there is more than one battery in one laptop, the percentage left is 
-    # available for each battery separately, although state and remaining 
-    # time for overall block is shown in the status of the first battery 
+    # if there is more than one battery in one laptop, the percentage left is
+    # available for each battery separately, although state and remaining
+    # time for overall block is shown in the status of the first battery
     batteries = status.split("\n")
     state_batteries = []
     commasplitstatus_batteries = []
     percentleft_batteries = []
     time = ""
+    timeleft = None
     for battery in batteries:
         if battery != '':
             state_batteries.append(battery.split(": ")[1].split(", ")[0])
             commasplitstatus = battery.split(", ")
             if not time:
+                timeleft = ""
                 time = commasplitstatus[-1].strip()
                 # check if it matches a time
                 time = re.match(r"(\d+):(\d+)", time)
                 if time:
                     time = ":".join(time.groups())
                     timeleft = " ({})".format(time)
-                else:
-                    timeleft = ""
 
             p = int(commasplitstatus[1].rstrip("%\n"))
             if p > 0:
@@ -57,26 +101,6 @@ else:
         percentleft = int(sum(percentleft_batteries) / len(percentleft_batteries))
     else:
         percentleft = 0
-
-    # stands for charging
-    FA_LIGHTNING = wrap_span("\uf0e7", "yellow")
-
-    # stands for plugged in
-    FA_PLUG = wrap_span('\uf1e6')
-
-    # stands for using battery
-    FA_BATTERY = wrap_span('\uf240')
-
-    # stands for unknown status of battery
-    FA_QUESTION = wrap_span('\uf128')
-
-    FA_BATTERY_LIST = [
-        "\uf244",  # empty
-        "\uf243",  # 1 quarter
-        "\uf242",  # half
-        "\uf241",  # 3 quarters
-        "\uf240",  # full
-    ]
 
     # deceiver state of batteries, if one is unknown take the state of the other
     unknowntext = FA_QUESTION + " " + FA_BATTERY + " "
@@ -96,29 +120,7 @@ else:
         else:
             fulltext += FA_LIGHTNING + " " + FA_PLUG + " "
 
-
-    def color(percent):
-        if percent < 10:
-            # exit code 33 will turn background red
-            return "#FFFFFF"
-        if percent < 20:
-            return "#FF3300"
-        if percent < 30:
-            return "#FF6600"
-        if percent < 40:
-            return "#FF9900"
-        if percent < 50:
-            return "#FFCC00"
-        if percent < 60:
-            return "#FFFF00"
-        if percent < 70:
-            return "#FFFF33"
-        if percent < 80:
-            return "#FFFF66"
-        return "#FFFFFF"
-
-
-    form = '<span color="{}">{}%</span>'
+    form = '<span col="{}">{}%</span>'
     fulltext += form.format(color(percentleft), percentleft)
     fulltext += timeleft
 
--- tests/test_battery2.py
+++ tests/test_battery2.py
@@ -1,7 +1,40 @@
-from i3_battery_block import wrap_span
+import pytest
+
+from i3_battery_block.battery import wrap_span, color
+
 
 def test_wrap():
-    assert wrap_span("a") == "<span font='FontAwesome'>a</span>", "String should be wraped into span element!"
+    assert wrap_span("a") == "<span font='FontAwesome'>a</span>", "String should be wrapped into span element!"
+
 
 def test_wrap_with_color():
-    assert wrap_span("a", "b") == "<span font='FontAwesome' color='b'>a</span>", "String should be wraped into span element!"
+    assert wrap_span("a",
+                     "b") == "<span font='FontAwesome' col='b'>a</span>", "String should be wrapped into span " \
+                                                                            "element! "
+@pytest.mark.parametrize(
+    "smaller_then, expected",
+    [
+        (10, "#FFFFFF"),
+        (20, "#FF3300"),
+        (30, "#FF6600"),
+        (40, "#FF9900"),
+        (50, "#FFCC00"),
+        (60, "#FFFF00"),
+        (70, "#FFFF33"),
+        (80, "#FFFF66"),
+        (100, "#FFFFFF")
+    ]
+)
+def test_color(smaller_then: int, expected: str):
+    assert color(smaller_then - 1) == expected, "below threshold %s color %s is expected" % (smaller_then, expected)
+
+
+@pytest.mark.parametrize(
+    "not_percentage",
+    [
+        -50, 150,
+    ]
+)
+def test_color_percentage_over_stepped(not_percentage):
+    with pytest.raises(AttributeError, match=r'Threshold.*'):
+        color(not_percentage)
