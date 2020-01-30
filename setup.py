7713bd1ecce76c556faccf34047066581ecd65e7
Volker Goehler <volker.goehler@informatik.tu-freiberg.de>
2020-01-30 14:19:34 +0100

    added python 3.6 for ci

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Topic :: Utilities',
@@ -63,7 +64,7 @@ setup(
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
